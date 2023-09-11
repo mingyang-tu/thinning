223270a7e77a95b20a531028e93bb01d461d44bf
MY Tu <tu890626@gmail.com>
2023-09-11 21:50:15 +0800

    complete condition 1 & 2

--- src/thinning/arcelli_baja.py
+++ src/thinning/arcelli_baja.py
@@ -3,6 +3,8 @@ import numpy as np
 from .distance_transform import inner_contour
 
 
+# FIXME: isolated point
+# TODO: change into class
 def arcelli_baja(binary):       # input : np.uint8
     ROW, COL = binary.shape
     inner = binary.copy()
@@ -13,21 +15,25 @@ def arcelli_baja(binary):       # input : np.uint8
         for i in range(ROW):
             for j in range(COL):
                 if transformed[i, j] == 1:
-                    # TODO: condition1 and condition2
-                    pass
+                    if condition1(transformed, i, j) or condition2(transformed, i, j):
+                        output[i, j] = 1
+        inner = (transformed == 2).astype(np.uint8)
+        if not np.any(inner):
+            break
+    return output
 
 
 def condition1(transformed, i, j):
-    ROW, COL = transformed.shape
+    ROW, COL = transformed.shape[0] - 1, transformed.shape[1] - 1
     if 0 < i < ROW:
         if (
-            ((transformed[i-1, j] == 0) and (transformed[i+1, j] == 2)) or 
+            ((transformed[i-1, j] == 0) and (transformed[i+1, j] == 2)) or
             ((transformed[i-1, j] == 2) and (transformed[i+1, j] == 0))
         ):
             return False
     if 0 < j < COL:
         if (
-            ((transformed[i, j-1] == 0) and (transformed[i, j+1] == 2)) or 
+            ((transformed[i, j-1] == 0) and (transformed[i, j+1] == 2)) or
             ((transformed[i, j-1] == 2) and (transformed[i, j+1] == 0))
         ):
             return False
@@ -35,5 +41,33 @@ def condition1(transformed, i, j):
 
 
 def condition2(transformed, i, j):
-    # TODO: condition2
-    pass
+    ROW, COL = transformed.shape[0] - 1, transformed.shape[1] - 1
+    if (i > 0) and (j > 0):
+        if (
+            (transformed[i-1, j-1] == 1) and
+            (transformed[i-1, j] == 0) and
+            (transformed[i, j-1] == 0)
+        ):
+            return True
+    if (i > 0) and (j < COL):
+        if (
+            (transformed[i-1, j+1] == 1) and
+            (transformed[i-1, j] == 0) and
+            (transformed[i, j+1] == 0)
+        ):
+            return True
+    if (i < ROW) and (j > 0):
+        if (
+            (transformed[i+1, j-1] == 1) and
+            (transformed[i+1, j] == 0) and
+            (transformed[i, j-1] == 0)
+        ):
+            return True
+    if (i < ROW) and (j < COL):
+        if (
+            (transformed[i+1, j+1] == 1) and
+            (transformed[i+1, j] == 0) and
+            (transformed[i, j+1] == 0)
+        ):
+            return True
+    return False
