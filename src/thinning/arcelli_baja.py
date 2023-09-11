c6da2ac0a4eddd88b71a334c6231355470ac09e2
MY Tu <tu890626@gmail.com>
2023-09-11 22:50:42 +0800

    change into class

--- src/thinning/arcelli_baja.py
+++ src/thinning/arcelli_baja.py
@@ -3,71 +3,72 @@ import numpy as np
 from .distance_transform import inner_contour
 
 
-# FIXME: isolated point
-# TODO: change into class
-def arcelli_baja(binary):       # input : np.uint8
-    ROW, COL = binary.shape
-    inner = binary.copy()
-    output = np.zeros((ROW, COL), dtype=np.uint8)
+class ArcelliBaja:
+    def __init__(self, binary):     # input : np.uint8
+        self.binary = binary
+        self.row, self.col = self.binary.shape
+        self.r1 = self.row - 1
+        self.c1 = self.col - 1
 
-    while True:
-        transformed = inner_contour(inner)
-        for i in range(ROW):
-            for j in range(COL):
-                if transformed[i, j] == 1:
-                    if condition1(transformed, i, j) or condition2(transformed, i, j):
-                        output[i, j] = 1
-        inner = (transformed == 2).astype(np.uint8)
-        if not np.any(inner):
-            break
-    return output
+    # FIXME: isolated point
+    def run(self):
+        inner = self.binary.copy()
+        output = np.zeros((self.row, self.col), dtype=np.uint8)
 
+        while True:
+            transformed = inner_contour(inner)
+            for i in range(self.row):
+                for j in range(self.col):
+                    if transformed[i, j] == 1:
+                        if self.condition1(transformed, i, j) or self.condition2(transformed, i, j):
+                            output[i, j] = 1
+            inner = (transformed == 2).astype(np.uint8)
+            if not np.any(inner):
+                break
+        return output
 
-def condition1(transformed, i, j):
-    ROW, COL = transformed.shape[0] - 1, transformed.shape[1] - 1
-    if 0 < i < ROW:
-        if (
-            ((transformed[i-1, j] == 0) and (transformed[i+1, j] == 2)) or
-            ((transformed[i-1, j] == 2) and (transformed[i+1, j] == 0))
-        ):
-            return False
-    if 0 < j < COL:
-        if (
-            ((transformed[i, j-1] == 0) and (transformed[i, j+1] == 2)) or
-            ((transformed[i, j-1] == 2) and (transformed[i, j+1] == 0))
-        ):
-            return False
-    return True
+    def condition1(self, transformed, i, j):
+        if 0 < i < self.r1:
+            if (
+                ((transformed[i-1, j] == 0) and (transformed[i+1, j] == 2)) or
+                ((transformed[i-1, j] == 2) and (transformed[i+1, j] == 0))
+            ):
+                return False
+        if 0 < j < self.c1:
+            if (
+                ((transformed[i, j-1] == 0) and (transformed[i, j+1] == 2)) or
+                ((transformed[i, j-1] == 2) and (transformed[i, j+1] == 0))
+            ):
+                return False
+        return True
 
-
-def condition2(transformed, i, j):
-    ROW, COL = transformed.shape[0] - 1, transformed.shape[1] - 1
-    if (i > 0) and (j > 0):
-        if (
-            (transformed[i-1, j-1] == 1) and
-            (transformed[i-1, j] == 0) and
-            (transformed[i, j-1] == 0)
-        ):
-            return True
-    if (i > 0) and (j < COL):
-        if (
-            (transformed[i-1, j+1] == 1) and
-            (transformed[i-1, j] == 0) and
-            (transformed[i, j+1] == 0)
-        ):
-            return True
-    if (i < ROW) and (j > 0):
-        if (
-            (transformed[i+1, j-1] == 1) and
-            (transformed[i+1, j] == 0) and
-            (transformed[i, j-1] == 0)
-        ):
-            return True
-    if (i < ROW) and (j < COL):
-        if (
-            (transformed[i+1, j+1] == 1) and
-            (transformed[i+1, j] == 0) and
-            (transformed[i, j+1] == 0)
-        ):
-            return True
-    return False
+    def condition2(self, transformed, i, j):
+        if (i > 0) and (j > 0):
+            if (
+                (transformed[i-1, j-1] == 1) and
+                (transformed[i-1, j] == 0) and
+                (transformed[i, j-1] == 0)
+            ):
+                return True
+        if (i > 0) and (j < self.c1):
+            if (
+                (transformed[i-1, j+1] == 1) and
+                (transformed[i-1, j] == 0) and
+                (transformed[i, j+1] == 0)
+            ):
+                return True
+        if (i < self.r1) and (j > 0):
+            if (
+                (transformed[i+1, j-1] == 1) and
+                (transformed[i+1, j] == 0) and
+                (transformed[i, j-1] == 0)
+            ):
+                return True
+        if (i < self.r1) and (j < self.c1):
+            if (
+                (transformed[i+1, j+1] == 1) and
+                (transformed[i+1, j] == 0) and
+                (transformed[i, j+1] == 0)
+            ):
+                return True
+        return False
