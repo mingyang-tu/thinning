5505c2d970f8043d3a0517f01da50c1c9d7a2236
MY Tu <tu890626@gmail.com>
2023-10-31 18:49:49 +0800

    merge files

--- src/thinning/arcelli_baja.py
+++ src/thinning/arcelli_baja.py
@@ -1,8 +1,6 @@
 import numpy as np
 from numpy.typing import NDArray
 
-from .distance_transform import four_distance_transform
-
 
 class ArcelliBaja:
     def __init__(self, binary: NDArray[np.uint8]):
@@ -27,15 +25,13 @@ class ArcelliBaja:
 
     def condition1(self, transformed, i, j):
         if 0 < i < self.r1:
-            if (
-                ((transformed[i-1, j] == 0) and (transformed[i+1, j] > self.level)) or
-                ((transformed[i-1, j] > self.level) and (transformed[i+1, j] == 0))
+            if ((transformed[i - 1, j] == 0) and (transformed[i + 1, j] > self.level)) or (
+                (transformed[i - 1, j] > self.level) and (transformed[i + 1, j] == 0)
             ):
                 return False
         if 0 < j < self.c1:
-            if (
-                ((transformed[i, j-1] == 0) and (transformed[i, j+1] > self.level)) or
-                ((transformed[i, j-1] > self.level) and (transformed[i, j+1] == 0))
+            if ((transformed[i, j - 1] == 0) and (transformed[i, j + 1] > self.level)) or (
+                (transformed[i, j - 1] > self.level) and (transformed[i, j + 1] == 0)
             ):
                 return False
         return True
@@ -43,30 +39,47 @@ class ArcelliBaja:
     def condition2(self, transformed, i, j):
         if (i > 0) and (j > 0):
             if (
-                (transformed[i-1, j-1] == self.level) and
-                (transformed[i-1, j] == 0) and
-                (transformed[i, j-1] == 0)
+                (transformed[i - 1, j - 1] == self.level)
+                and (transformed[i - 1, j] == 0)
+                and (transformed[i, j - 1] == 0)
             ):
                 return True
         if (i > 0) and (j < self.c1):
             if (
-                (transformed[i-1, j+1] == self.level) and
-                (transformed[i-1, j] == 0) and
-                (transformed[i, j+1] == 0)
+                (transformed[i - 1, j + 1] == self.level)
+                and (transformed[i - 1, j] == 0)
+                and (transformed[i, j + 1] == 0)
             ):
                 return True
         if (i < self.r1) and (j > 0):
             if (
-                (transformed[i+1, j-1] == self.level) and
-                (transformed[i+1, j] == 0) and
-                (transformed[i, j-1] == 0)
+                (transformed[i + 1, j - 1] == self.level)
+                and (transformed[i + 1, j] == 0)
+                and (transformed[i, j - 1] == 0)
             ):
                 return True
         if (i < self.r1) and (j < self.c1):
             if (
-                (transformed[i+1, j+1] == self.level) and
-                (transformed[i+1, j] == 0) and
-                (transformed[i, j+1] == 0)
+                (transformed[i + 1, j + 1] == self.level)
+                and (transformed[i + 1, j] == 0)
+                and (transformed[i, j + 1] == 0)
             ):
                 return True
         return False
+
+
+def four_distance_transform(binary: NDArray[np.uint8]):
+    ROW, COL = binary.shape
+    output = binary.copy()
+
+    for i in range(1, ROW):
+        for j in range(1, COL):
+            if output[i, j] != 0:
+                output[i, j] = min(output[i, j - 1], output[i - 1, j]) + 1
+
+    for i in range(ROW - 2, -1, -1):
+        for j in range(COL - 2, -1, -1):
+            if output[i, j] != 0:
+                output[i, j] = min(min(output[i, j + 1], output[i + 1, j]) + 1, output[i, j])
+
+    return output
